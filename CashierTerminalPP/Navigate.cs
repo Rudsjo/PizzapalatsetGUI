1c50315ff27876dca7914d6ff062b75f85146452
Marcus Welin <marcuswelin88@gmail.com>
2020-03-26 11:22:23 +0100

    Base for usercontrol
    Första basen med userControl för Cashier.

---
+++ CashierTerminalPP/Navigate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashierTerminalPP
+{
+    public enum Navigate
+    {
+        Login = 0,
+        Base = 1,
+    };
+}
---
+++ CashierTerminalPP/ViewModel/LoginPageViewModel.cs
@@ -0,0 +1,117 @@
+﻿using BackendHandler;
+using CashierTerminalPP.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+
+namespace CashierTerminalPP.ViewModel
+{
+    class LoginPageViewModel : INotifyPropertyChanged
+    {
+        public static IDatabase rep { get; set; } = Helpers.GetSelectedBackend();
+        public RelayCommand Update { get; set; }
+        public RelayCommand Login { get; set; }
+
+        public PageRelayCommand CashierPageCommand { get; set; }
+        public PageRelayCommand LoginPageCommand { get; set; }
+
+
+        #region Properties
+        private object selectedViewModel;
+        public object SelectedViewModel
+        {
+            get { return selectedViewModel; }
+            set
+            {
+                if (selectedViewModel == null)
+                    value = new CashierPageViewModel();
+
+                selectedViewModel = value;
+                OnPropertyChanged("SelectedViewModel");
+            }
+        }
+
+
+        private Navigate switchView;
+        public Navigate SwitchView
+        {
+            get { return switchView; }
+            set
+            {
+                switchView = value;
+                OnPropertyChanged("SwitchView");
+            }
+        }
+
+
+        private string userID;
+        public string UserID
+        {
+            get { return userID; }
+            set
+            {
+                userID = value;
+                OnPropertyChanged("UserID");
+            }
+        }
+
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+        #endregion
+        public LoginPageViewModel()
+        {
+            CashierPageCommand = new PageRelayCommand(ShowCashierPage);
+            LoginPageCommand = new PageRelayCommand(ShowLoginPage);
+        }
+
+        private async void ShowCashierPage(object p)
+        {
+
+            SwitchView = Navigate.Base;
+            SelectedViewModel = new CashierPageViewModel();
+            //var id = int.TryParse(UserID, out int validInput);
+            //var result = await rep.CheckUserIdAndPassword(validInput, Password);
+
+            //if (result.Item1 == true)
+            //{
+            //    SwitchView = Navigate.Base;
+            //    SelectedViewModel = new CashierPageViewModel();
+            //    UserID = "";
+            //    Password = "";
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Fel användarnamn eller lösenord");
+            //}
+
+        }
+
+        private void ShowLoginPage(object p)
+        {
+            SwitchView = Navigate.Login;
+            SelectedViewModel = new LoginPageViewModel();
+            SelectedViewModel = null;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
