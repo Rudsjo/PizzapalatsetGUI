859c46087fa136fba35904dfa481c280c1436e5f
Marcus Dogramaci Svanberg <marcus_haha@live.se>
2020-04-01 12:54:05 +0200

    Ny push något gick fel

---
+++ WpfInfoTerminal/WpfInfoTerminal/ObservableObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WpfInfoTerminal
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged<T>(ref T prop, T value, [CallerMemberName] string propertyChanged = "")
+        {
+            prop = value;
+
+            var handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyChanged));
+            }
+        }
+    }
+}
