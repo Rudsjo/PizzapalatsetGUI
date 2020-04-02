de417d8392a558f1dc4a74516303e728492b1442
stimp <stimpon@gmail.com>
2020-04-02 07:23:55 +0200

    First commit

---
+++ TerminalServer/TerminalServer/ProgramState.cs
@@ -0,0 +1,18 @@
+﻿namespace TerminalServer
+{
+    /// <summary>
+    /// Class that keeps track of the program
+    /// </summary>
+    public class ProgramState
+    {
+        /// <summary>
+        /// Is the program running
+        /// </summary>
+        public static bool Running { get; set; }
+
+        /// <summary>
+        /// Single instance of the ServerHandlerClass
+        /// </summary>
+        public static ServerHandler Server { get; set; }
+    }
+}
